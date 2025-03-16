3ac45863ce33b3eac8aa8f5153a1dad1013e91f4
Himanshu Nishad <himanshunishad620@gmail.com>
2025-03-16 14:18:21 +0530

    bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Board from "./components/Board";
-
+// import bg from "./assets/bg.jpeg"
 export default function App() {
+  // console.log(bg)
   return (
-    <div className={`w-screen bg-[url(bg.jpeg)] bg-cover bg-content h-screen flex-col flex justify-center items-center`}>
+    <div className={`w-screen bg-[url('/bg.jpeg')] bg-cover bg-content h-screen flex-col flex justify-center items-center`}>
       <Board />
     </div>
   );
