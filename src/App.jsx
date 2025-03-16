4ce3816e8c0a5b8d003ca60d3cd0125686a1fd21
Himanshu Nishad <himanshunishad620@gmail.com>
2025-03-16 14:12:50 +0530

    bg imgae

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Board from "./components/Board";
 
 export default function App() {
   return (
-    <div className={`w-screen bg-[url(D:/Technokrate/flip-card-game/public/bg.jpeg)] bg-cover bg-content h-screen flex-col flex justify-center items-center`}>
+    <div className={`w-screen bg-[url(bg.jpeg)] bg-cover bg-content h-screen flex-col flex justify-center items-center`}>
       <Board />
     </div>
   );
